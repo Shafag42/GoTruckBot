9f1e0edd136e182cad3e6ca92661c52d6982e3fe
unknown <shefeqa24@gmail.com>
2023-09-18 19:51:44 +0400

    modifiyed bot for to respond only specific group

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ FROM python:3.11
 WORKDIR /Telegram Bot
 
 # Copy the necessary files into the container
-COPY GoTruck_bot.py .
+COPY GoTruckBot.py .
 COPY requirements.txt .
 COPY .env .
 
@@ -13,4 +13,4 @@ COPY .env .
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Run the bot
-CMD [ "python", "GoTruck_bot.py" ]
+CMD [ "python", "GoTruckBot.py" ]
--- GoTruckBot.py
+++ GoTruckBot.py
@@ -1,11 +1,10 @@
-import pymongo
 import asyncio
 import os
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
 import motor.motor_asyncio
 from aiogram import types
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -39,7 +38,7 @@ async def get_order_data():
 
 
 async def get_group_id():
-    group_username = "GoTruckTest"
+    group_username = "GoTruckGroup"
 
     # Get the group info using the group username
     invite_link_info = await bot.get_chat("@" + group_username)
@@ -49,12 +48,11 @@ async def get_group_id():
     return group_id
 
 
-async def start_command():
-    message_text = "Salamlar. Bota xoş gəlmisiniz! Buradaki mavi sözə /orders toxunaraq ən son sifarişi görə bilərsiniz."
-
-    group_id = await get_group_id()
-    await bot.send_message(group_id, message_text)
-
+async def start_command(message: types.Message):
+    # Check if the message is from a group or supergroup
+    if message.chat.type in ['group', 'supergroup']:
+        message_text = "Salamlar. Bota xoş gəlmisiniz! /Buraya toxunaraq ən son sifarişi görə bilərsiniz."
+        await message.answer(message_text)
 
 async def orders_command():
     last_order_id = None  # Initial value, no orders sent yet
@@ -86,37 +84,36 @@ async def orders_command():
                 group_id = await get_group_id()
                 await bot.send_message(group_id, message_text)
 
+async def check_orders(message: types.Message):
+    if message.chat.type in ['group', 'supergroup']:
+        # Check for new orders when a user calls /orders command
+        order_data = await get_order_data()
 
-async def check_orders():
-    # Check for new orders when a user calls /orders command
-    order_data = await get_order_data()
+        if order_data:
+            latest_order = order_data[-1]
+            origin = latest_order.get('origin', 'N/A')
+            destination = latest_order.get('destination', 'N/A')
+            trucktype = latest_order.get('trucktype', 'N/A')
+            minprice = latest_order.get('minpayment', 'N/A')
+            maxprice = latest_order.get('maxpayment', 'N/A')
+            phone = latest_order.get('number', 'N/A')
 
-    if order_data:
-        latest_order = order_data[-1]
-        origin = latest_order.get('origin', 'N/A')
-        destination = latest_order.get('destination', 'N/A')
-        trucktype = latest_order.get('trucktype', 'N/A')
-        minprice = latest_order.get('minpayment', 'N/A')
-        maxprice = latest_order.get('maxpayment', 'N/A')
-        phone = latest_order.get('number', 'N/A')
+            message_text = f"Son sifariş:\n🗺 Götürüləcək ünvan:{origin}\n🚍 Çatdırılacaq ünvan: {destination}\n🚛 Qoşqunun növü: {trucktype}\n💰 Minimum qiymət: {minprice}\n💰 Maksimum qiymət: {maxprice}\n📞 Əlaqə nömrəsi: {phone}"
 
-        message_text = f"Son sifariş:\n🗺 Götürüləcək ünvan:{origin}\n🚍 Çatdırılacaq ünvan: {destination}\n🚛 Qoşqunun növü: {trucktype}\n💰 Minimum qiymət: {minprice}\n💰 Maksimum qiymət: {maxprice}\n📞 Əlaqə nömrəsi: {phone}"
+            group_id = await get_group_id()
+            await bot.send_message(group_id, message_text)
 
-        group_id = await get_group_id()
-        await bot.send_message(group_id, message_text)
+        else:
+            message_text = "Hələki yeni sifariş yoxdur.Təşəkkürlər."
 
-    else:
-
-        message_text = "Hələki yeni sifariş yoxdur.Təşəkkürlər."
-
-        group_id = await get_group_id()
-        await bot.send_message(group_id, message_text)
+            group_id = await get_group_id()
+            await bot.send_message(group_id, message_text)
 
 
 async def main():
     # Add the command handlers
     dp.register_message_handler(start_command, commands=["start"])
-    dp.register_message_handler(check_orders, commands=["orders"])
+    dp.register_message_handler(check_orders, commands=["orders","Buraya"])
 
     # Start the background task to check for new orders and send messages to the group
     asyncio.create_task(orders_command())
